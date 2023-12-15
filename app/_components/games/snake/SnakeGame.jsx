f4fec408233ea7397b9ef21a7a48aeb813909fd0
Sebastian Medina <snmedina91@gmail.com>
2023-12-15 16:21:02 -0300

    add dificulty and db to game

--- app/_components/games/snake/SnakeGame.jsx
+++ app/_components/games/snake/SnakeGame.jsx
@@ -1,12 +1,41 @@
 'use client'
 import React, { useState, useEffect, useRef } from "react";
 import "./SnakeGame.css"; // Importa tu archivo CSS de estilos Tailwind
+import { createClient, kv } from "@vercel/kv";
 
 const SnakeGame = () => {
   const [snake, setSnake] = useState([{ x: 0, y: 0 }]);
   const [food, setFood] = useState({ x: 5, y: 5 });
   const [direction, setDirection] = useState("RIGHT");
   const [score, setScore] = useState(0);
+  const [name, setName] = useState("");
+  const [gameRunning, setGameRunning] = useState(false);
+  const [scores, setScores] = useState([]); 
+  const [level, setLevel] = useState(1);
+
+  const client = createClient({
+    url: "https://subtle-stallion-49535.kv.vercel-storage.com",
+    token:
+      "AcF_ASQgOTNiZGQ0ZTItYmQ1Yi00NDE1LTk2YjQtMGY2YmM1MzY4NDYyMzQ1YjI0OTgxZGFkNGM3NWFlMTRkYzAyMzI1Y2ZjYTY=",
+  });
+
+  const startGame = () => {
+    setSnake([{ x: 0, y: 0 }]);
+    setFood(getRandomPosition());
+    setDirection("RIGHT");
+    setScore(0);
+    setLevel(1);
+    setGameRunning(true);
+  };
+
+
+  const saveGame = async () => {
+    const key = Date.now();
+    await client.set(`snakeGames:${key}`, { name, score });
+    // Fetch all scores after saving a new game
+    fetchAllScores();
+    return `snakeGames:${key}`;
+  };
 
   const canvasSize = 10;
   const cellSize = 30;
@@ -102,6 +131,9 @@ const SnakeGame = () => {
       setFood(getRandomPosition());
       setDirection("RIGHT");
       setScore(0);
+      setGameRunning(false);
+      setLevel(1); 
+      saveGame();
     } else {
       newSnake.unshift(head);
 
@@ -109,6 +141,9 @@ const SnakeGame = () => {
         // Snake ate the food - increase score and place new food
         setScore(score + 1);
         setFood(getRandomPosition());
+        if (score > 0 && (score + 1) % 3 === 0) {
+          setLevel(level + 1);
+        }
       } else {
         // Remove the last segment of the snake if it didn't eat food
         newSnake.pop();
@@ -153,26 +188,73 @@ const SnakeGame = () => {
   }, [snake, food]);
 
   useEffect(() => {
-    // Move the snake periodically
-    const intervalId = setInterval(() => moveSnake(), 200);
+    // Move the snake periodically only if the game is running
+    if (gameRunning) {
+      const intervalId = setInterval(() => moveSnake(), 200 - level * 20);;
+      // Clean up interval on component unmount
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [snake, direction, food, score, gameRunning]);
 
-    // Clean up interval on component unmount
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [snake, direction, food, score]);
+  const fetchAllScores = async () => {
+    const keys = await client.keys("snakeGames:*");
+    const allScores = await Promise.all(
+      keys.map(async (key) => {
+        const game = await client.get(key);
+        return {
+          key: key,
+          name: game.name,
+          score: game.score,
+        };
+      })
+    );
+
+    allScores.sort((a, b) => b.score - a.score); // Sort scores in descending order
+    setScores(allScores);
+  };
+
+  useEffect(() => {
+    // Fetch scores when the component mounts
+    fetchAllScores();
+  }, []);
 
   return (
     <div className="flex items-center flex-col mt-4">
-    <h1>Snake Game</h1>
-    <label>Score: {score}</label>
-    <canvas
-      ref={canvasRef}
-      width={canvasSize * cellSize}
-      height={canvasSize * cellSize}
-      className="border border-gray-500"
-    />
-  </div>
+      <h1>Snake Game</h1>
+      {!gameRunning && (
+        <>
+          <label>Enter your name:</label>
+          <input
+            type="text"
+            placeholder="Your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-2 p-2 border"
+          />
+          <button onClick={startGame} className="mt-2 text-white">
+            Start
+          </button>
+        </>
+      )}
+      {gameRunning && <label>Score: {score}</label>}
+      <canvas
+        ref={canvasRef}
+        width={canvasSize * cellSize}
+        height={canvasSize * cellSize}
+        className="border border-gray-500"
+      />
+      <h2 className="text-white">All Scores:</h2>
+      <ul className="text-white">
+        {scores.map((game) => (
+          <li key={game.key}>
+            {`${game.name}: ${game.score}`}
+          </li>
+        ))}
+      </ul>
+    </div>
+
   );
 };
 
